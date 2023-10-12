1b0e6903e528ec01417db2ab24b8aeac28211ccc
Faces <suhya990312@gmail.com>
2023-10-12 14:23:48 +0900

    213

--- Assets/2. Game_Stage1/Dead_Stage1.cs
+++ Assets/2. Game_Stage1/Dead_Stage1.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Dead_Stage1 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Game_Stage1");
+        }
     }
 }
